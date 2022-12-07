e65b40b574d89e78a29d4c3ffabbb85b6d7dd642
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-12-07 16:23:56 -0600

    build: next font variable set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import { Ubuntu } from '@next/font/google'
+
+const ubuntu = Ubuntu({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-ubuntu',
+})
+
 export default function Home() {
   return (
     <h1 className="text-3xl font-bold underline">Hello world!</h1>
