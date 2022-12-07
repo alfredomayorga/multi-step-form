27bc6708762bb22348e610af3a58ada4514fbc37
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-12-07 16:37:03 -0600

    build: design system
    Colors and font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,27 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontSize: {
+      body: ['12px', 'auto'],
+      'body-md': ['14px', 'auto'],
+      'body-lg': ['16px', '25px'],
+      heading: ['32px', 'auto'],
+    },
+    colors: {
+      purple: '#483EFF',
+      error: '#EE374A',
+      'light-bg': '#EFF5FF',
+      'border-color': '#D6D9E6',
+      demin: '#022959',
+      gray: '#9699AA',
+      'light-blue': '#ABBCFF',
+      'light-grey': '#D6D9E6',
+      orange: '#FFAF7E',
+      pink: '#F9818E',
+      'sky-blue': '#BEE2FD',
+      'white-two': '#F8F9FF',
+      white: '#FFFFFF',
+    },
     extend: {
       fontFamily: {
         sans: ['var(--font-inter)', ...fontFamily.sans],
