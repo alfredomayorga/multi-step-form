33c4d2f879cf0a684b55f036098221ac33ef5e42
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-12-22 13:11:51 -0600

    feat: input component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Ubuntu } from '@next/font/google'
-import { Button } from '../components/ui'
+import { Button, Input } from '../components/ui'
 
 const ubuntu = Ubuntu({
   weight: ['400', '700'],
@@ -9,7 +9,7 @@ const ubuntu = Ubuntu({
 
 export default function Home() {
   return (
-    <div className="flex">
+    <div className={ubuntu.className + "flex"}>
       <Button
         type="primary"
         onClick={() => {}}
@@ -18,6 +18,23 @@ export default function Home() {
         type="secondary"
         onClick={()=> {}}
       />
+      <div className="flex flex-col">
+        <Input 
+          type="text"
+          placeholder="e.g Stephen King"
+          labelText='Name'
+        /> 
+        <Input 
+          type="email"
+          placeholder="e.g sking@lorem.com"
+          labelText='Email'
+        /> 
+        <Input 
+          type="tel"
+          placeholder="e.g +972 123 4321"
+          labelText='Phone Number'
+        /> 
+      </div>
     </div>
   )
 }
