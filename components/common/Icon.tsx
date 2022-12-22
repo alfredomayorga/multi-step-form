9124e94aa516848109c19d858707de07695c6f9f
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-12-22 16:24:42 -0600

    feat: steps icon component

---
+++ components/common/Icon.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+interface IconProps {
+  color: string
+  icon: string
+}
+export function Icon({ color, icon }: IconProps) {
+  return (
+    <Image
+      src={icon}
+      width={40}
+      height={40}
+      alt="Plan Icon"
+      className={`bg-{color}`}
+    />
+  )
+}
